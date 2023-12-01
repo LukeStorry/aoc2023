7618aea0458e60dd6e150c9f6f62539d0392cd53
Luke Storry <luke.storry@letsdothis.com>
2023-12-01 11:59:05 +0000

    Tidy and fix edgecase in ts solution

--- day1/solve.ts
+++ day1/solve.ts
@@ -1,3 +1,4 @@
+import { keys, sum, values } from "lodash";
 import { solve } from "../runner/typescript";
 
 const numbers = {
@@ -11,35 +12,24 @@ const numbers = {
   seven: "7",
   eight: "8",
   nine: "9",
-  "1": "1",
-  "2": "2",
-  "3": "3",
-  "4": "4",
-  "5": "5",
-  "6": "6",
-  "7": "7",
-  "8": "8",
-  "9": "9",
 };
+
+function getValue(line: string): number {
+  const digits = line.match(/\d/g);
+  return Number(digits.at(0) + digits.at(-1));
+}
+
+function getValueWithWords(line: string): number {
+  const regexString = `(${[...keys(numbers), ...values(numbers)].join("|")})`;
+  const first = line.match(new RegExp(regexString))[1];
+  const last = line.match(new RegExp(`.*${regexString}`))[1];
+  return Number(`${numbers[first] ?? first}${numbers[last] ?? last}`);
+}
 
 solve({
   parser: (input) => input.split("\n"),
-  part1: (lines) =>
-    lines
-      .map((line) => {
-        const digits = line.match(/\d/g);
-        return Number(digits.at(0) + digits.at(-1));
-      })
-      .reduce((a, b) => a + b, 0),
-  part2: (lines) =>
-    lines
-      .map((line) => {
-        const regex = new RegExp(`${Object.keys(numbers).join("|")}`, "g");
-        const digits = line.match(regex);
-        return Number(numbers[digits.at(0)] + numbers[digits.at(-1)]);
-      })
-      .reduce((a, b) => a + b, 0),
-  // part2: part2_recursive,
+  part1: (lines) => sum(lines.map(getValue)),
+  part2: (lines) => sum(lines.map(getValueWithWords)),
 
   test1Input: "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet",
   test1Output: 142,
@@ -49,22 +39,23 @@ solve({
 });
 
 /** Original bleary-eyed solution when I couldn't get the regex to work for part 2
-    I can always count on recursion to save the day, especially early on */
-function part2_recursive(input: string[]): number {
-  function recursiveFind(s: string) {
-    if (!s) return [];
+    I can always count on recursion to save the day, especially early on 
+    Update: turns out this only works as I was lucky with my input - new recursive solution works better
+    */
+// function part2_recursive(input: string[]): number {
+//   function recursiveFind(s: string) {
+//     if (!s) return [];
 
-    for (const [key, value] of Object.entries(numbers)) {
-      if (s.startsWith(key))
-        return [value, ...recursiveFind(s.replace(key, ""))];
-    }
-    return [...recursiveFind(s.slice(1))];
-  }
+//     for (const [key, value] of Object.entries(numbers)) {
+//       if (s.startsWith(key))
+//         return [value, ...recursiveFind(s.replace(key, ""))];
+//     }
+//     return [...recursiveFind(s.slice(1))];
+//   }
 
-  return input
-    .map((line) => {
-      const digits = recursiveFind(line);
-      return Number(digits.at(0) + digits.at(-1));
-    })
-    .reduce((a, b) => a + b, 0);
-}
+//   const getValue = (line: string): number => {
+//     const digits = recursiveFind(line);
+//     return Number(digits.at(0) + digits.at(-1));
+//   };
+//   return sum(input.map(getValue));
+// }
