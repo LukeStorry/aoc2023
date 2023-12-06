880dde3a61a35a6ef3ffcb9459cbb25ab7fc1034
Luke Storry <luke.storry@letsdothis.com>
2023-12-06 11:50:47 +0000

    inline & tidy tsc

--- day6/solve.ts
+++ day6/solve.ts
@@ -1,36 +1,22 @@
 import { solve } from "../runner/typescript";
 import { zip } from "lodash";
 
-function parser(input: string) {
-  return input
-    .trim()
-    .split("\n")
-    .map((l) => l.match(/\d+/g).map(Number));
-}
-
-function getWins([time, distance]: [number, number]): number {
-  const holds = [...Array(time).keys()];
-  const distances = holds.map((hold) => (time - hold) * hold);
-  const wins = distances.filter((d) => d > distance);
-  return wins.length;
-}
-
-function part1([times, distances]: number[][]): number {
-  return zip(times, distances)
-    .map(getWins)
-    .reduce((a, b) => a * b);
-}
-
-function part2([times, distances]: number[][]): number {
-  const time = Number(times.join(""));
-  const distance = Number(distances.join(""));
-  return getWins([time, distance]);
-}
+const getWins = ([totalTime, record]) =>
+  [...Array(totalTime).keys()]
+    .map((timeHolding) => (totalTime - timeHolding) * timeHolding)
+    .filter((distance) => distance > record).length;
 
 solve({
-  parser,
-  part1,
-  part2,
-  part1Tests: [["Time:      7  15   30\n    Distance:  9  40  200\n    ", 288]],
+  parser: (input) => input.split("\n").map((l) => l.match(/\d+/g).map(Number)),
+
+  part1: ([times, records]) =>
+    zip(times, records)
+      .map(getWins)
+      .reduce((a, b) => a * b),
+
+  part2: ([times, distances]) =>
+    getWins([Number(times.join("")), Number(distances.join(""))]),
+
+  part1Tests: [["Time:      7  15   30\n    Distance:  9  40  200", 288]],
   part2Tests: [["Time:      7  15   30\nDistance:  9  40  200", 71503]],
 });
