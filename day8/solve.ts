647186653735cb73ce83953a0ac186d26a02af9c
Luke Storry <luke.storry@letsdothis.com>
2023-12-08 09:13:30 +0000

    day 8 part 1

---
+++ day8/solve.ts
@@ -0,0 +1,52 @@
+import { solve } from "../runner/typescript";
+
+type P = {
+  directions: (0 | 1)[];
+  nodesMap: Map<string, readonly [string, string]>;
+};
+function parser(input: string): P {
+  const directions = input
+    .split("\n")[0]
+    .split("")
+    .map((lr) => (lr === "L" ? 0 : 1));
+
+  const elements = Array.from(input.matchAll(/(\w{3}).*(\w{3}).*(\w{3})/g)).map(
+    ([_, name, left, right]) => [name, [left, right]] as const
+  );
+
+  const nodesMap = new Map(elements);
+
+  return { directions, nodesMap };
+}
+
+function part1({ directions, nodesMap }: P): number {
+  let step = 0;
+  let currentNode = "AAA";
+  while (currentNode !== "ZZZ") {
+    const direction = directions[step % directions.length];
+    currentNode = nodesMap.get(currentNode)[direction];
+    step++;
+  }
+  return step;
+}
+
+function part2({ directions, nodesMap }: P): number {
+  return 0;
+}
+
+solve({
+  parser: parser,
+  part1: part1,
+  // part2: part2,
+
+  part1Tests: [
+    ["RL\n\nAAA = (BBB, CCC)\nBBB = (DDD, EEE)\nCCC = (ZZZ, GGG)", 2],
+    ["LLR\n\nAAA = (BBB, BBB)\nBBB = (AAA, ZZZ)\nZZZ = (ZZZ, ZZZ)", 6],
+  ],
+  part2Tests: [
+    [
+      "LR\n\n11A = (11B, XXX)\n11B = (XXX, 11Z)\n11Z = (11B, XXX)\n22A = (22B, XXX)\n22B = (22C, 22C)\n22C = (22Z, 22Z)\n22Z = (22B, 22B)\nXXX = (XXX, XXX)",
+      6,
+    ],
+  ],
+});
