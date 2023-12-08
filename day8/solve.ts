0a5d22426cea8d6e90f511374e61796706df6e5e
Luke Storry <luke.storry@letsdothis.com>
2023-12-08 09:57:33 +0000

    tidy p2

--- day8/solve.ts
+++ day8/solve.ts
@@ -1,23 +1,21 @@
 import { solve } from "../runner/typescript";
 import { lowestCommonMultiple } from "../utils";
+
 type P = { directions: string; nodes: Record<string, [string, string]> };
 function parser(input: string): P {
   const directions = input.split("\n")[0];
-  const nodes = {};
-  const matches = input.matchAll(/(\w{3}).*(\w{3}).*(\w{3})/g);
-  for (const [_, name, left, right] of matches) {
-    nodes[name] = [left, right];
-  }
+  const nodes = Array.from(input.matchAll(/(\w{3}).*(\w{3}).*(\w{3})/g)).reduce(
+    (acc, [_, name, left, right]) => ({ ...acc, [name]: [left, right] }),
+    {}
+  );
   return { directions, nodes };
 }
 
 function followMap(start, end, { directions, nodes }: P): number {
-  let current = start;
-  let step = 0;
+  let [current, step] = [start, 0];
   while (!current.endsWith(end)) {
     const direction = directions[step % directions.length] === "L" ? 0 : 1;
-    current = nodes[current][direction];
-    step++;
+    [current, step] = [nodes[current][direction], step + 1];
   }
   return step;
 }
@@ -26,16 +24,16 @@ function part1(input: P) {
   return followMap("AAA", "ZZZ", input);
 }
 
-function part2(input: P): number {
+function part2(input: P) {
   let startNodes = Object.keys(input.nodes).filter((n) => n.endsWith("A"));
-  const stepsUntilZ = startNodes.map((start) => followMap(start, "Z", input));
-  return stepsUntilZ.reduce(lowestCommonMultiple);
+  const steps = startNodes.map((start) => followMap(start, "Z", input));
+  return steps.reduce(lowestCommonMultiple);
 }
 
 solve({
-  parser: parser,
-  part1: part1,
-  part2: part2,
+  parser,
+  part1,
+  part2,
   part1Tests: [
     ["RL\n\nAAA = (BBB, CCC)\nBBB = (DDD, EEE)\nCCC = (ZZZ, GGG)", 2],
     ["LLR\n\nAAA = (BBB, BBB)\nBBB = (AAA, ZZZ)\nZZZ = (ZZZ, ZZZ)", 6],
