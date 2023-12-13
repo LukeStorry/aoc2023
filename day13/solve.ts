b727bfd3e9bce1455e2db1722b1d5d42d62c5ebe
Luke Storry <luke.storry@letsdothis.com>
2023-12-13 13:20:50 +0000

    13.2

--- day13/solve.ts
+++ day13/solve.ts
@@ -1,89 +1,72 @@
 import { solve } from "../runner/typescript";
-import { sum, uniq } from "lodash";
+import { cloneDeep, reverse, sum } from "lodash";
 
 type Grid = string[][];
 function parser(input: string): Grid[] {
   return input.split("\n\n").map((g) => g.split("\n").map((l) => l.split("")));
 }
 
-function getReflections(line: string[]) {
-  const len = line.length;
-  const reflections: number[] = [];
-  for (let i = 1; i < len; i++) {
-    const right = line
-      .slice(i, i * 2)
-      .reverse()
-      .join("");
-
-    const left = line.slice(i - right.length, i).join("");
-
-    if (left === right) {
-      reflections.push(i);
-    }
+function getMirror(
+  grid: Grid,
+  direction: "vertical" | "horizontal",
+  ignore?: number
+): number {
+  if (direction === "horizontal") {
+    grid = grid[0].map((_, i) => grid.map((line) => line[i]));
   }
-  return reflections;
+  const possiblePerLine = grid.map((line) =>
+    line
+      .map((_, i) => {
+        const left = line.slice(i - Math.min(i, line.length - i), i).join("");
+        const right = reverse(line.slice(i, i * 2)).join("");
+        return left === right ? i : null;
+      })
+      .filter(Boolean)
+  );
+
+  return possiblePerLine[0].find((m) =>
+    possiblePerLine.every((set) => set.includes(m) && m !== ignore)
+  );
 }
 
-function func(grid: Grid) {
-  // console.log(grid.map((l) => l.join("")));
+function summarise(grid: Grid, ignore?: number) {
+  return (
+    getMirror(grid, "vertical", ignore) ??
+    getMirror(grid, "horizontal", ignore / 100) * 100
+  );
+}
 
-  const rotated = grid[0].map((_, i) => grid.map((line) => line[i]));
+function fixSmudgeAndSummarise(grid: Grid): number {
+  const originalSummary = summarise(grid);
 
-  const verticals = grid.map(getReflections);
-  const horizontals = rotated.map(getReflections);
-  // console.log(verticals, horizontals);
-
-  let total = 0;
-
-  for (const possibleVertical of uniq(verticals.flat())) {
-    if (verticals.every((v) => v.includes(possibleVertical))) {
-      // total += possibleVertical;
-      // console.log("vertical", possibleVertical);
-      return possibleVertical;
-    }
-  }
-
-  for (const possibleHorizontal of uniq(horizontals.flat())) {
-    if (horizontals.every((v) => v.includes(possibleHorizontal))) {
-      // total += possibleHorizontal * 100;
-      // console.log("horizontal", possibleHorizontal);
-
-      return possibleHorizontal * 100;
-    }
-  }
-  console.log("none");
-  return total;
+  return grid
+    .flatMap((line, y) =>
+      line.map((_, x) => {
+        const newGrid = cloneDeep(grid);
+        newGrid[y][x] = grid[y][x] === "#" ? "." : "#";
+        return newGrid;
+      })
+    )
+    .map((possible) => summarise(possible, originalSummary))
+    .find(Boolean);
 }
 
 function part1(grids: Grid[]): number {
-  return sum(grids.map(func));
+  return sum(grids.map(summarise));
 }
 
-function part2(values: any[]): any[] {
-  function func2(a) {
-    return a;
-  }
-
-  const out1 = func2(values[0]);
-
-  console.log(out1);
-
-  return values.map(func2);
+function part2(grids: Grid[]): number {
+  return sum(grids.map(fixSmudgeAndSummarise));
 }
+
+const testInput =
+  "#.##..##.\n..#.##.#.\n##......#\n##......#\n..#.##.#.\n..##..##.\n#.#.##.#.\n\n#...##..#\n#....#..#\n..##..###\n#####.##.\n#####.##.\n..##..###\n#....#..#";
 
 solve({
   parser,
   part1,
-  // part2,
+  part2,
 
-  part1Tests: [
-    [
-      "#.##..##.\n..#.##.#.\n##......#\n##......#\n..#.##.#.\n..##..##.\n#.#.##.#.\n\n#...##..#\n#....#..#\n..##..###\n#####.##.\n#####.##.\n..##..###\n#....#..#",
-      405,
-    ],
-  ],
-  part2Tests: [
-    // ["aaa", 0],
-    // ["a", 0],
-  ],
+  part1Tests: [[testInput, 405]],
+  part2Tests: [[testInput, 400]],
 });
