2966b86a07d049f8ee884c61d2cb765812dccde7
Luke Storry <luke.storry@letsdothis.com>
2023-12-21 18:29:17 +0000

    tidy & move to tuples

--- day21/solve.ts
+++ day21/solve.ts
@@ -1,162 +1,99 @@
 import { countBy, fromPairs, groupBy, sum, toPairs, values, zip } from "lodash";
 import { solve } from "../runner/typescript";
 
-type Point = [number, number] | number[];
-type MultiGridPoint = { pointInGrid: Point; gridOffset: Point };
-type Data = {
-  width: number;
-  height: number;
-  start: MultiGridPoint;
-  graph: Record<string, Point[]>;
-};
+type Point = [number, number, number, number];
+type Grid = string[][];
+type Data = { start: Point; grid: Grid };
 
 function parser(input: string): Data {
-  const grid: string[][] = input.split("\n").map((row) => row.split(""));
-  const start = grid
-    .flatMap((row, y) =>
-      row.map((cell, x) =>
-        cell == "S"
-          ? ({
-              pointInGrid: [x, y],
-              gridOffset: [0, 0],
-            } satisfies MultiGridPoint)
-          : null
-      )
-    )
-    .find(Boolean);
-
-  const cellsWithConnections = grid
-    .flatMap((row, y) =>
-      row.map((cell, x) => {
-        if (cell == "#") return null;
-        const connections = zip([-1, 0, 1, 0], [0, -1, 0, 1])
-          .map(([dx, dy]) => [x + dx, y + dy] satisfies Point)
-          .filter(([nx, ny]) => "S.".includes(grid[ny]?.[nx]));
-        return [`${x},${y}`, connections];
-      })
-    )
-    .filter(Boolean);
-
-  return {
-    start,
-    width: grid[0].length,
-    height: grid.length,
-    graph: fromPairs(cellsWithConnections),
-  };
+  const grid = input.split("\n").map((row) => row.split(""));
+  const start = grid.flatMap((row, y) =>
+    row
+      .map((cell, x) => (cell == "S" ? ([x, y, 0, 0] satisfies Point) : null))
+      .filter(Boolean)
+  )[0];
+  return { grid, start };
 }
 
-function finalDestinations(
-  { graph, width, height, start }: Data,
-  steps: number
-): MultiGridPoint[] {
-  // keys work as a Set, but with cheap access to full object.
-  let points = { [`${start.gridOffset}|${start.pointInGrid}`]: start };
+function getNext([x, y, offsetX, offsetY]: Point, grid: Grid): Point[] {
+  const [width, height] = [grid[0].length, grid.length];
+  const sameGrid = zip([-1, 0, 1, 0], [0, -1, 0, 1])
+    .map(([dx, dy]) => [x + dx, y + dy, offsetX, offsetY] satisfies Point)
+    .filter(([nx, ny]) => "S.".includes(grid[ny]?.[nx]));
+  const nextGrid = [
+    x === 0 ? ([width - 1, y, offsetX - 1, offsetY] satisfies Point) : null,
+    x === width - 1 ? ([0, y, offsetX + 1, offsetY] satisfies Point) : null,
+    y === 0 ? ([x, height - 1, offsetX, offsetY - 1] satisfies Point) : null,
+    y === height - 1 ? ([x, 0, offsetX, offsetY + 1] satisfies Point) : null,
+  ].filter(Boolean);
 
+  return [...sameGrid, ...nextGrid];
+}
+
+function finalDestinations(start: Point, grid: Grid, steps: number): Point[] {
+  let points = { [`${start}`]: start };
   for (let i = 0; i < steps; i++) {
-    const nextPoints = values(points).flatMap(
-      ({ pointInGrid: [x, y], gridOffset: [offsetX, offsetY] }) => {
-        const sameGrid: MultiGridPoint[] = graph[`${x},${y}`].map(
-          (neighbor) => ({
-            pointInGrid: neighbor,
-            gridOffset: [offsetX, offsetY],
-          })
-        );
-
-        const nextGrid: MultiGridPoint[] = [
-          x === 0
-            ? {
-                pointInGrid: [width - 1, y],
-                gridOffset: [offsetX - 1, offsetY],
-              }
-            : null,
-          x === width - 1
-            ? {
-                pointInGrid: [0, y],
-                gridOffset: [offsetX + 1, offsetY],
-              }
-            : null,
-          y === 0
-            ? {
-                pointInGrid: [x, height - 1],
-                gridOffset: [offsetX, offsetY - 1],
-              }
-            : null,
-          y === height - 1
-            ? {
-                pointInGrid: [x, 0],
-                gridOffset: [offsetX, offsetY + 1],
-              }
-            : null,
-        ].filter(Boolean);
-
-        const result = [...sameGrid, ...nextGrid];
-        return result;
-      }
-    );
-
-    points = fromPairs(
-      nextPoints.map((p) => [
-        `${p.pointInGrid.map(Number)}|${p.gridOffset.map(Number)}`,
-        p,
-      ])
-    );
+    // if (i % 10 === 0) console.log(`${i}/${steps}`);
+    const nextPoints = values(points).flatMap((p) => getNext(p, grid));
+    points = fromPairs(nextPoints.map((p) => [`${p}`, p]));
   }
-
   return values(points);
 }
 
-function part1(data: Data, isTest: boolean) {
+function part1({ start, grid }: Data, isTest: boolean) {
   const steps = isTest ? 6 : 64;
-  const visited = finalDestinations(data, steps);
+  const visited = finalDestinations(start, grid, steps);
   return visited.length;
 }
 
-function part2(data: Data, isTest: boolean) {
-  const steps = isTest ? 5000 : 26501365;
-  const repeats = Math.floor(steps / data.width);
-  const hopefullyEnoughSteps = data.width * 2 + (steps % data.width);
+function part2({ start, grid }: Data, isTest: boolean) {
+  const width = grid[0].length;
+  const gridsToFill = isTest ? 3 : 2; // cycles to run until stablilises
+  const totalSteps = isTest ? 5000 : 26501365;
+  const hopefullyEnoughSteps = width * gridsToFill + (totalSteps % width);
 
-  const visited = finalDestinations(data, hopefullyEnoughSteps);
+  const visited = finalDestinations(start, grid, hopefullyEnoughSteps);
 
   const gridCounts = fromPairs(
-    toPairs(groupBy(visited, ({ gridOffset: [_, y] }) => y)).map(
-      ([offsetY, values]) => [
-        offsetY,
-        countBy(values, ({ gridOffset: [x, _] }) => x),
-      ]
+    toPairs(groupBy(visited, ([_, __, ___, yOffset]) => yOffset)).map(
+      ([offsetY, values]) => {
+        const rowCount = countBy(values, ([_, __, xOffset]) => xOffset);
+        return [offsetY, rowCount];
+      }
     )
   );
 
   const corners = [
-    gridCounts[-2][0],
-    gridCounts[2][0],
-    gridCounts[0][-2],
-    gridCounts[0][2],
+    gridCounts[-gridsToFill][0],
+    gridCounts[gridsToFill][0],
+    gridCounts[0][-gridsToFill],
+    gridCounts[0][gridsToFill],
   ];
 
   const mostlyEmptyEdges = [
-    gridCounts[-2][-1],
-    gridCounts[-2][1],
-    gridCounts[2][-1],
-    gridCounts[2][1],
+    gridCounts[-gridsToFill][-1],
+    gridCounts[-gridsToFill][1],
+    gridCounts[gridsToFill][-1],
+    gridCounts[gridsToFill][1],
   ];
 
   const mostlyFullEdges = [
-    gridCounts[-1][-1],
-    gridCounts[-1][1],
-    gridCounts[1][-1],
-    gridCounts[1][1],
+    gridCounts[-1][1 - gridsToFill],
+    gridCounts[-1][gridsToFill - 1],
+    gridCounts[1][1 - gridsToFill],
+    gridCounts[1][gridsToFill - 1],
   ];
 
-  const evenFullGrids = gridCounts[0][0];
-  const oddFullGrids = gridCounts[0][1];
+  const evenFullGrids = gridCounts[0][gridsToFill % 2];
+  const oddFullGrids = gridCounts[0][(gridsToFill % 2) - 1];
 
+  const cycleSize = Math.floor(totalSteps / width);
   return (
     sum(corners) +
-    sum(mostlyEmptyEdges) * repeats +
-    sum(mostlyFullEdges) * (repeats - 1) +
-    oddFullGrids * repeats ** 2 +
-    evenFullGrids * (repeats - 1) ** 2
+    sum(mostlyEmptyEdges) * cycleSize +
+    sum(mostlyFullEdges) * (cycleSize - 1) +
+    oddFullGrids * cycleSize ** 2 +
+    evenFullGrids * (cycleSize - 1) ** 2
   );
 }
 
@@ -167,5 +104,5 @@ solve({
     "...........\n.....###.#.\n.###.##..#.\n..#.#...#..\n....#.#....\n.##..S####.\n.##..#...#.\n.......##..\n.##.#.####.\n.##..##.##.\n...........",
   part2,
   part1Tests: [[, 16]],
-  // part2Tests: [[, 16733044]],
+  part2Tests: [[, 16733044]],
 });
