311312b61df828ecfca168fe469cbc92c142ba2d
Luke Storry <luke.storry@letsdothis.com>
2023-12-23 17:58:25 +0000

    23.2 finally

--- day23/solve.ts
+++ day23/solve.ts
@@ -1,96 +1,87 @@
 import { solve } from "../runner/typescript";
-import { entries, keys, maxBy, values } from "lodash";
-type Grid = string[][];
+import { entries, fromPairs, maxBy, values } from "lodash";
 type Node = [number, number, number];
 type Graph = { [key: string]: Node[] };
 const directions = { "^": [0, -1], ">": [1, 0], v: [0, 1], "<": [-1, 0] };
 
-function makeGraph(grid: string[][], slippery: boolean): Graph {
-  const graph = {} as Graph;
-  grid.forEach((row, y) => {
-    row.forEach((cell, x) => {
-      if (cell === "#") return;
-      const deltas =
-        slippery && cell in directions
-          ? [directions[cell]]
-          : values(directions);
-      const next = deltas
-        .map(([dx, dy]) => [x + dx, y + dy])
-        .filter(([nx, ny]) => !!grid[ny]?.[nx] && grid[ny][nx] != "#")
-        .map(([nx, ny]) => [nx, ny, 1] satisfies Node);
-      graph[`${x},${y}`] = next;
-    });
-  });
+function makeGraph(input: string, canWalkUpSteep = false): Graph {
+  const grid = input.split("\n").map((row) => row.split(""));
 
-  // make new compressed graph of just just junctions
-  const compressedGraph = {} as Graph;
-  const junctions = entries(graph).filter(([_, next]) => next.length != 2);
-  junctions.forEach(([name, nexts]) => {
-    const newNextNodes: Node[] = [];
-    for (const next of nexts) {
-      let [nx, ny] = next;
-      const seen = new Set<string>([name]);
-      while (true) {
-        let weight = 1;
-        const nextName = `${nx},${ny}`;
-        seen.add(nextName);
-        let nextNexts = graph[nextName];
-        if (nextNexts.length != 2) {
-          newNextNodes.push([nx, ny, weight] satisfies Node);
-          break;
-        }
-        [nx, ny] = nextNexts.find(([x, y]) => !seen.has(`${x},${y}`));
-        weight += 1;
-      }
+  function getConnectionsForCell(x: number, y: number): [number, number][] {
+    const cell = grid[y][x];
+    const deltas =
+      cell in directions && !canWalkUpSteep
+        ? [directions[cell]]
+        : values(directions);
+    return deltas
+      .map(([dx, dy]) => [x + dx, y + dy])
+      .filter(([nx, ny]) => !!grid[ny]?.[nx] && grid[ny][nx] != "#")
+      .map(([nx, ny]) => [nx, ny]);
+  }
+  const cells = grid.flatMap((row, y) =>
+    row
+      .map((cell, x) => [x, y, cell] as const)
+      .filter(([_, __, cell]) => cell !== "#");
+      
+  const fullGraph = fromPairs(
+    cells
+        .map(([x, y]) => [`${x},${y}`, getConnectionsForCell(x, y)])
+    )
+  );
+
+  function followConnectionUntilNextJunction(
+    name: string,
+    oldConnection: [number, number]
+  ) {
+    let [nx, ny] = oldConnection;
+    let weight = 1;
+    const seen = new Set<string>([name]);
+    while (true) {
+      const name = `${nx},${ny}`;
+      let nextConnection = fullGraph[name];
+      if (nextConnection.length != 2) return [nx, ny, weight] satisfies Node;
+      [nx, ny] = nextConnection.find(([x, y]) => !seen.has(`${x},${y}`));
+      weight += 1;
+      seen.add(name);
     }
-    compressedGraph[name] = newNextNodes;
-  });
+  }
 
-  console.log({
-    total: keys(graph).length,
-    compressed: keys(compressedGraph).length,
-  });
-
-  return graph;
+  // make new compressed graph of just junctions
+  const compressedGraph = fromPairs(
+    entries(fullGraph)
+      .filter(([_, connections]) => connections.length != 2)
+      .map(([name, connections]) => [
+        name,
+        connections.map((c) => followConnectionUntilNextJunction(name, c)),
+      ])
+  );
+  return compressedGraph;
 }
 
 function findLongest(graph: Graph): number {
   const end = maxBy(values(graph).flat(), ([_, y]) => y);
-  let longestPath = 0;
-
-  const queue: [number, number, number, string][] = [[1, 0, 0, ""]];
-
+  let longestPath = -1;
+  const queue: [Node, string[]][] = [[[1, 0, 0], []]];
   while (queue.length) {
-    const [x, y, pathLength, visited] = queue.shift();
+    const [[x, y, pathLength], visited] = queue.pop();
+
     if (x === end[0] && y === end[1]) {
-      console.log(pathLength, queue.length);
-      if (pathLength > longestPath) longestPath = pathLength;
+      longestPath = Math.max(longestPath, pathLength);
       continue;
     }
 
-    graph[`${x},${y}`]
-      .filter(([nx, ny, _]) => !visited.includes(`|${nx},${ny}|`))
-      .forEach(([x, y, weight]) => {
-        queue.push([x, y, pathLength + weight, `${visited}|${x},${y}`]);
-      });
+    for (const [nx, ny, weight] of graph[`${x},${y}`]) {
+      const name = `${nx},${ny}`;
+      if (visited.includes(name)) continue;
+      queue.push([[nx, ny, pathLength + weight], visited.concat(name)]);
+    }
   }
   return longestPath;
 }
 
-function part1(grid: Grid): number {
-  const graph = makeGraph(grid, true);
-  return findLongest(graph);
-}
-
-function part2(grid: Grid): number {
-  const graph = makeGraph(grid, false);
-  return findLongest(graph);
-}
-
 solve({
-  parser: (input) => input.split("\n").map((row) => row.split("")),
-  part1,
-  part2,
+  part1: (input: string) => findLongest(makeGraph(input)),
+  part2: (input: string) => findLongest(makeGraph(input, true)),
 
   testInput:
     "#.#####################\n#.......#########...###\n#######.#########.#.###\n###.....#.>.>.###.#.###\n###v#####.#v#.###.#.###\n###.>...#.#.#.....#...#\n###v###.#.#.#########.#\n###...#.#.#.......#...#\n#####.#.#.#######.#.###\n#.....#.#.#.......#...#\n#.#####.#.#.#########v#\n#.#...#...#...###...>.#\n#.#.#v#######v###.###v#\n#...#.>.#...>.>.#.###.#\n#####v#.#.###v#.#.###.#\n#.....#...#...#.#.#...#\n#.#########.###.#.#.###\n#...###...#...#...#.###\n###.###.#.###v#####v###\n#...#...#.#.>.>.#.>.###\n#.###.###.#.###.#.#v###\n#.....###...###...#...#\n#####################.#",
