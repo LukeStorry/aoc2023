36690b8f4ffcdeb0d4c47a3256dbe53f9bc08725
Luke Storry <luke.storry@letsdothis.com>
2023-12-23 17:58:14 +0000

    23.2 graph compression

--- day23/solve.ts
+++ day23/solve.ts
@@ -1,5 +1,5 @@
 import { solve } from "../runner/typescript";
-import { maxBy, values } from "lodash";
+import { entries, keys, maxBy, values } from "lodash";
 type Grid = string[][];
 type Node = [number, number, number];
 type Graph = { [key: string]: Node[] };
@@ -16,10 +16,39 @@ function makeGraph(grid: string[][], slippery: boolean): Graph {
           : values(directions);
       const next = deltas
         .map(([dx, dy]) => [x + dx, y + dy])
-        .filter(([nx, ny]) => !!grid[ny]?.[nx] && grid[ny][nx] !== "#")
+        .filter(([nx, ny]) => !!grid[ny]?.[nx] && grid[ny][nx] != "#")
         .map(([nx, ny]) => [nx, ny, 1] satisfies Node);
       graph[`${x},${y}`] = next;
     });
+  });
+
+  // make new compressed graph of just just junctions
+  const compressedGraph = {} as Graph;
+  const junctions = entries(graph).filter(([_, next]) => next.length != 2);
+  junctions.forEach(([name, nexts]) => {
+    const newNextNodes: Node[] = [];
+    for (const next of nexts) {
+      let [nx, ny] = next;
+      const seen = new Set<string>([name]);
+      while (true) {
+        let weight = 1;
+        const nextName = `${nx},${ny}`;
+        seen.add(nextName);
+        let nextNexts = graph[nextName];
+        if (nextNexts.length != 2) {
+          newNextNodes.push([nx, ny, weight] satisfies Node);
+          break;
+        }
+        [nx, ny] = nextNexts.find(([x, y]) => !seen.has(`${x},${y}`));
+        weight += 1;
+      }
+    }
+    compressedGraph[name] = newNextNodes;
+  });
+
+  console.log({
+    total: keys(graph).length,
+    compressed: keys(compressedGraph).length,
   });
 
   return graph;
@@ -34,7 +63,7 @@ function findLongest(graph: Graph): number {
   while (queue.length) {
     const [x, y, pathLength, visited] = queue.shift();
     if (x === end[0] && y === end[1]) {
-      console.log(pathLength);
+      console.log(pathLength, queue.length);
       if (pathLength > longestPath) longestPath = pathLength;
       continue;
     }
@@ -60,8 +89,8 @@ function part2(grid: Grid): number {
 
 solve({
   parser: (input) => input.split("\n").map((row) => row.split("")),
-  part1: part1,
-  part2: part2,
+  part1,
+  part2,
 
   testInput:
     "#.#####################\n#.......#########...###\n#######.#########.#.###\n###.....#.>.>.###.#.###\n###v#####.#v#.###.#.###\n###.>...#.#.#.....#...#\n###v###.#.#.#########.#\n###...#.#.#.......#...#\n#####.#.#.#######.#.###\n#.....#.#.#.......#...#\n#.#####.#.#.#########v#\n#.#...#...#...###...>.#\n#.#.#v#######v###.###v#\n#...#.>.#...>.>.#.###.#\n#####v#.#.###v#.#.###.#\n#.....#...#...#.#.#...#\n#.#########.###.#.#.###\n#...###...#...#...#.###\n###.###.#.###v#####v###\n#...#...#.#.>.>.#.>.###\n#.###.###.#.###.#.#v###\n#.....###...###...#...#\n#####################.#",
