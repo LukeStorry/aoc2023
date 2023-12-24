5b3e5a109f2b9628554a1f0365190de3a12061ee
Luke Storry <luke.storry@letsdothis.com>
2023-12-24 10:31:50 +0000

    24.1

--- day24/solve.ts
+++ day24/solve.ts
@@ -1,77 +1,34 @@
 import { solve } from "../runner/typescript";
-import { max, sum, groupBy } from "lodash";
+import { fromPairs, zip } from "lodash";
 
-type HailStone = [
-  px: number,
-  py: number,
-  pz: number,
-  vx: number,
-  vy: number,
-  vz: number,
-];
-type Point = [x: number, y: number];
+const hailstoneKeys = ["x", "y", "z", "dx", "dy", "dz"] as const;
+type HailStone = { [key in (typeof hailstoneKeys)[number]]: number };
 
 function parser(input: string): HailStone[] {
-  return input
-    .split("\n")
-    .map((line) => line.split(/,|@/).map(Number) as HailStone);
-}
-
-type LineSegment = readonly [start: Point, end: Point];
-
-function getLineSegmentInside2DZone(
-  stone: HailStone,
-  [min, max]: [number, number]
-): LineSegment {
-  const [px, py, _, vx, vy, __] = stone;
-  const m = vy / vx;
-  const b = py - m * px;
-
-  const intersections: Point[] = [
-    [px, py], // current position
-    [min, m * min + b], // left boundary
-    [max, m * max + b], // right boundary
-    [(min - b) / m, min], // bottom boundary
-    [(max - b) / m, max], // top boundary
-  ];
-
-  const validIntersections = intersections.filter(
-    ([x, y]) =>
-      x >= min &&
-      x <= max &&
-      y >= min &&
-      y <= max && // inside the zone
-      ((vx > 0 && x >= px) || (vx < 0 && x <= px)) && // in the future x direction
-      ((vy > 0 && y >= py) || (vy < 0 && y <= py)) // in the future y direction
-  );
-
-  // validIntersections.sort((a, b) => (a.x - px) * vx + (a.y - py) * vy);
-
-  const start: Point = validIntersections[0];
-  const end: Point = validIntersections.at(-1);
-
-  return [start, end] as LineSegment;
+  return input.split("\n").map((line) => {
+    const values = line.split(/,|@/).map(Number);
+    return fromPairs(zip(hailstoneKeys, values)) as HailStone;
+  });
 }
 
 function intersect(
-  [[aStartX, aStartY], [aEndX, aEndY]]: LineSegment,
-  [[bStartX, bStartY], [bEndX, bEndY]]: LineSegment
+  stoneA: HailStone,
+  stoneB: HailStone,
+  [min, max]: [number, number]
 ): boolean {
-  const det =
-    (aEndX - aStartX) * (bEndY - bStartY) -
-    (bEndX - bStartX) * (aEndY - aStartY);
-  if (det === 0) return false;
-  const lambda =
-    ((bEndY - bStartY) * (bEndX - aStartX) +
-      (bStartX - bEndX) * (bEndY - aStartY)) /
-    det;
+  const slopeA = stoneA.dy / stoneA.dx;
+  const interceptA = stoneA.y - slopeA * stoneA.x;
+  const slopeB = stoneB.dy / stoneB.dx;
+  const interceptB = stoneB.y - slopeB * stoneB.x;
 
-  const gamma =
-    ((aStartY - aEndY) * (bEndX - aStartX) +
-      (aEndX - aStartX) * (bEndY - aStartY)) /
-    det;
+  const x = (interceptB - interceptA) / (slopeA - slopeB);
+  const y = x * slopeA + interceptA;
 
-  return 0 < lambda && lambda < 1 && 0 < gamma && gamma < 1;
+  const isInFutureA = x > stoneA.x == stoneA.dx > 0;
+  const isInFutureB = x > stoneB.x == stoneB.dx > 0;
+  const isInsideZone = x >= min && x <= max && y >= min && y <= max;
+
+  return isInFutureA && isInFutureB && isInsideZone;
 }
 
 function part1(stones: HailStone[], isTest): number {
@@ -79,26 +36,10 @@ function part1(stones: HailStone[], isTest): number {
     ? [7, 27]
     : [200000000000000, 400000000000000];
 
-  const lineSegments = stones.map((stone) =>
-    getLineSegmentInside2DZone(stone, testArea)
-  );
   let intersections = 0;
   stones.forEach((stoneA, a) => {
-    const segmentA = lineSegments[a];
-    // const segmentA = getLineSegmentInside2DZone(stoneA, testArea);
     stones.slice(a + 1).forEach((stoneB, b) => {
-      const segmentB = lineSegments[a + b];
-      // const segmentB = getLineSegmentInside2DZone(stoneB, testArea);
-      if (!segmentA[0] || !segmentB[0]) return;
-      const doesIntersect = intersect(segmentA, segmentB);
-      if (doesIntersect) intersections += 1;
-      // console.log({
-      //   stoneA,
-      //   stoneB,
-      //   segmentA,
-      //   segmentB,
-      //   doesIntersect,
-      // });
+      if (intersect(stoneA, stoneB, testArea)) intersections += 1;
     });
   });
 
@@ -109,13 +50,10 @@ solve({
   parser: parser,
   part1,
   // onlyTests: true,
-  // part2: part2,
+  // part2,
   testInput:
     "19, 13, 30 @ -2,  1, -2\n18, 19, 22 @ -1, -1, -2\n20, 25, 34 @ -2, -2, -4\n12, 31, 28 @ -1, -2, -1\n20, 19, 15 @  1, -5, -3",
-  part1Tests: [
-    [, 2],
-    // ["a", 0],
-  ],
+  part1Tests: [[, 2]],
   part2Tests: [
     // ["aaa", 0],
     // ["a", 0],
